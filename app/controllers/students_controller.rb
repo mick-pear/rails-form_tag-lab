1dd182c1a7a67f9557fde234dfb87fbadf17bd74
Matthew <matthew_mcaleer@hotmail.com>
2022-06-22 20:08:57 +1000

    commit tree ac73c75b707290ab29bfa5da8d5fd6784302fcfc

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,14 @@
 class StudentsController < ApplicationController
   
-  def index
-    @students = Student.all
-  end
+    def index
+        @students = Student.all
+    end
 
-  def show
-    @student = Student.find(params[:id])
-  end
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def new
+    end   
 
 end
